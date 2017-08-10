8f35cc2be850493813ccb7d83a9224eaf69c26a5
Stefanie Gunderson <seedsofthesesame@gmail.com>
2017-08-10 09:05:46 -0700

    Add files via upload

---
+++ rmg-stef.rb
@@ -0,0 +1,27 @@
+# Welcome user with something fun and witty.
+puts "Welcome to Stef's Random Menu Generator!"
+puts "\nFood's about to get interesting..."
+puts "\nHere is todays menu; "
+
+# Arrays to create the menu.
+
+# Array 1 - Adjectives
+adj = ["Acerbic ", "Ambrosial ", "Blazed ", "Caustic ", "Classy ", "Dazzling ", "Gustatory ", "Heady ", "Insipid ", "Palatable "]
+
+# Array 2 - Cooking Styles
+styles = [" Fried ", " Charbroiled ", " Schwenkered ", " Coddled ", " Dum Pukht ", " Flambéd ", " Infused ", " En Papilloted ", " Hibachied ", " Macerated "]
+
+# Array 3 - Foods
+foods = [" Haggis.", " Kopi Luwak.", " Milt.", " Balut.", " Hákarl.", " Casu Marzu.", " Rocky Mountain Oysters.", " Escamoles.", " Shirako.", " Stargazey Pie."]
+
+# List (10) to create a full viewable menu.
+# Create a variable to use delete function in order to remove repetitiveness on the first array.
+10.times do |x|
+puts "\nMenu Item #{x+1}"
+  random_adj = adj.sample
+  adj.delete(random_adj)
+puts random_adj + styles.sample + foods.sample
+end
+
+# End note to user.
+puts "\nI hope you find something tasty from today's selections!"
